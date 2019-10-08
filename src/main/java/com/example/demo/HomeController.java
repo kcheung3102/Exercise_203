b1e1a65dfcb8a577787c9f26b1820d49f2c3733b
Kcheung3102 <Kcheung3102@gmail.com>
2019-10-07 23:19:11 -0400

    another commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
@@ -10,5 +12,11 @@ public class HomeController {
     public String loadFormPage(Model model ) {
         model.addAttribute("employee", new Employee());
         return "employeeform"
+    }
+
+    @PostMapping("/employeeform")
+    public String loadFormPage(@ModelAttribute Employee employee, Model model) {
+        model.addAttribute("employee", employee);
+        return "confirmemployee";
     }
 }
